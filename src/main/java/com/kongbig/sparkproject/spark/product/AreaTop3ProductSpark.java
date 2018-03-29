a5d763bec708041e3b9e310fb5c4adbf106f8bc3
kongbigliang <kongbigliang@163.com>
2018-03-29 14:20:41 +0800

    1.修改JavaRDD为JavaPairRDD 2.异构数据源之从MySQL中查询城市数据 3.关联城市信息以及RDD转换为DataFrame后注册临时表

--- src/main/java/com/kongbig/sparkproject/spark/product/AreaTop3ProductSpark.java
+++ src/main/java/com/kongbig/sparkproject/spark/product/AreaTop3ProductSpark.java
@@ -1,6 +1,7 @@
 package com.kongbig.sparkproject.spark.product;
 
 import com.alibaba.fastjson.JSONObject;
+import com.kongbig.sparkproject.conf.ConfigurationManager;
 import com.kongbig.sparkproject.constant.Constants;
 import com.kongbig.sparkproject.dao.ITaskDAO;
 import com.kongbig.sparkproject.dao.impl.DAOFactory;
@@ -8,11 +9,25 @@ import com.kongbig.sparkproject.domain.Task;
 import com.kongbig.sparkproject.util.ParamUtils;
 import com.kongbig.sparkproject.util.SparkUtils;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Describe: 各区域top3热门商品统计Spark作业
@@ -43,9 +58,18 @@ public class AreaTop3ProductSpark {
         String startDate = ParamUtils.getParam(taskParam, Constants.PARAM_START_DATE);
         String endDate = ParamUtils.getParam(taskParam, Constants.PARAM_END_DATE);
 
-        // 查询用户指定日期范围内的点击行为数据
-        JavaRDD<Row> clickActionRDD = getClickActionRDDByDate(sqlContext, startDate, endDate);
-        
+        // 查询用户指定日期范围内的点击行为数据（city_id，在哪个城市发生的点击行为）
+        // 技术点1：Hive数据源的使用
+        JavaPairRDD<Long, Row> cityId2ClickActionRDD = getCityId2ClickActionRDDByDate(
+                sqlContext, startDate, endDate);
+
+        // 从MySQL中查询城市信息
+        // 技术点2：异构数据源之MySQL的使用
+        JavaPairRDD<Long, Row> cityId2CityInfoRDD = getCityId2CityInfoRDD(sqlContext);
+
+        // 生成点击商品基础信息临时表
+        // 技术点3：将RDD转换为DataFrame，并注册成临时表
+        generateTempClickProductBasicTable(sqlContext, cityId2ClickActionRDD, cityId2CityInfoRDD);
 
         sc.stop();
     }
@@ -58,7 +82,7 @@ public class AreaTop3ProductSpark {
      * @param endDate    截至日期
      * @return 点击行为数据
      */
-    private static JavaRDD<Row> getClickActionRDDByDate(
+    private static JavaPairRDD<Long, Row> getCityId2ClickActionRDDByDate(
             SQLContext sqlContext, String startDate, String endDate) {
         /**
          * 从user_visit_action中查询用户访问行为数据。
@@ -74,7 +98,103 @@ public class AreaTop3ProductSpark {
                 + "AND action_time <= '" + endDate + "'";
 
         DataFrame clickActionDF = sqlContext.sql(sql);
-        return clickActionDF.javaRDD();
+
+        JavaRDD<Row> clickActionRDD = clickActionDF.javaRDD();
+
+        JavaPairRDD<Long, Row> cityId2clickActionRDD = clickActionRDD.mapToPair(
+                new PairFunction<Row, Long, Row>() {
+                    private static final long serialVersionUID = 6774903296601608485L;
+
+                    @Override
+                    public Tuple2<Long, Row> call(Row row) throws Exception {
+                        Long cityId = row.getLong(0);
+                        return new Tuple2<Long, Row>(cityId, row);
+                    }
+                });
+        return cityId2clickActionRDD;
+    }
+
+    /**
+     * 使用Spark SQL从MySQL中查询城市信息
+     *
+     * @param sqlContext SQLContext
+     * @return CityInfoRDD
+     */
+    private static JavaPairRDD<Long, Row> getCityId2CityInfoRDD(SQLContext sqlContext) {
+        // 构建MySQL连接配置信息(直接从配置文件中获取)
+        String url = null;
+        Boolean local = ConfigurationManager.getBoolean(Constants.SPARK_LOCAL);
+        if (local) {
+            url = ConfigurationManager.getProperty(Constants.JDBC_URL);
+        } else {
+            url = ConfigurationManager.getProperty(Constants.JDBC_USER_PROD);
+        }
+        Map<String, String> options = new HashMap<String, String>();
+        options.put("url", url);
+        options.put("dbtable", "city_info");
+
+        // 通过SQLContext去从MySQL中查询数据
+        DataFrame cityInfoDF = sqlContext.read().format("jdbc").options(options).load();
+
+        JavaRDD<Row> cityInfoRDD = cityInfoDF.javaRDD();
+        JavaPairRDD<Long, Row> cityId2cityInfoRDD = cityInfoRDD.mapToPair(
+                new PairFunction<Row, Long, Row>() {
+                    private static final long serialVersionUID = 2552393766313917627L;
+
+                    @Override
+                    public Tuple2<Long, Row> call(Row row) throws Exception {
+                        long cityId = row.getLong(0);
+                        return new Tuple2<Long, Row>(cityId, row);
+                    }
+                });
+        return cityId2cityInfoRDD;
+    }
+
+    /**
+     * 生成点击商品基础信息临时表
+     *
+     * @param sqlContext            SQLContext
+     * @param cityId2clickActionRDD 点击行为数据
+     * @param cityId2cityInfoRDD    城市数据
+     */
+    private static void generateTempClickProductBasicTable(
+            SQLContext sqlContext,
+            JavaPairRDD<Long, Row> cityId2clickActionRDD,
+            JavaPairRDD<Long, Row> cityId2cityInfoRDD) {
+        // 执行join操作，进行点击行为数据和城市数据的关联
+        JavaPairRDD<Long, Tuple2<Row, Row>> joinedRdd =
+                cityId2clickActionRDD.join(cityId2cityInfoRDD);
+
+        // 将上面的JavaPairRDD转换成JavaRDD<Row>（才能将RDD转换为DataFrame）
+        JavaRDD<Row> mappedRDD = joinedRdd.map(new Function<Tuple2<Long, Tuple2<Row, Row>>, Row>() {
+            private static final long serialVersionUID = -200524099028943510L;
+
+            @Override
+            public Row call(Tuple2<Long, Tuple2<Row, Row>> tuple) throws Exception {
+                long cityId = tuple._1;
+                Row clickAction = tuple._2._1;
+                Row cityInfo = tuple._2._2;
+
+                long productId = clickAction.getLong(1);
+                String cityName = cityInfo.getString(1);
+                String area = cityInfo.getString(2);
+
+                return RowFactory.create(cityId, cityName, area, productId);
+            }
+        });
+
+        // 基于JavaRDD<Row>的格式，就可以将其转换为DataFrame
+        List<StructField> structFields = new ArrayList<StructField>();
+        structFields.add(DataTypes.createStructField("city_id", DataTypes.LongType, true));
+        structFields.add(DataTypes.createStructField("city_name", DataTypes.StringType, true));
+        structFields.add(DataTypes.createStructField("area", DataTypes.StringType, true));
+        structFields.add(DataTypes.createStructField("product_id", DataTypes.LongType, true));
+
+        StructType schema = DataTypes.createStructType(structFields);
+        DataFrame df = sqlContext.createDataFrame(mappedRDD, schema);
+
+        // 将DataFrame中的数据，注册成临时表(tmp_clk_prod_base)
+        df.registerTempTable("tmp_clk_prod_base");
     }
 
 }
